1b0f02d465afba5d711eaf3ebbf0c8206e2cf792
Vafeen <666av6@gmail.com>
2025-02-01 12:26:42 +0300

    Update TradingPasswordActivityActions

--- presentation/src/main/java/ru/surf/learn2invest/presentation/ui/components/screens/trading_password/TradingPasswordActivityActions.kt
+++ presentation/src/main/java/ru/surf/learn2invest/presentation/ui/components/screens/trading_password/TradingPasswordActivityActions.kt
@@ -3,7 +3,7 @@ package ru.surf.learn2invest.presentation.ui.components.screens.trading_password
 enum class TradingPasswordActivityActions(
     val action: String
 ) {
-    CreateTradingPassword(action = CreateTradingPassword.name),
-    ChangeTradingPassword(action = ChangeTradingPassword.name),
-    RemoveTradingPassword(action = RemoveTradingPassword.name);
+    CreateTradingPassword(action = "CreateTradingPassword"),
+    ChangeTradingPassword(action = "ChangeTradingPassword"),
+    RemoveTradingPassword(action = "RemoveTradingPassword");
 }
